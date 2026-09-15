aef6aaa0fe08e68a6c5dea32d18587555b79e936
naappe <naappe@gmail.com>
2026-09-15 12:55:28 +0500

    Make PhoneHub launcher close old sessions first

--- PhoneHub.bat
+++ PhoneHub.bat
@@ -1,5 +1,17 @@
 @echo off
 title PhoneHub
+
+REM Close old PhoneHub tools/windows
+taskkill /IM PhoneHub.exe /F >nul 2>&1
+taskkill /IM scrcpy.exe /F >nul 2>&1
+taskkill /IM python.exe /F >nul 2>&1
+taskkill /IM pythonw.exe /F >nul 2>&1
+
+REM Start ADB clean
+adb start-server >nul 2>&1
+
+REM Open only main PhoneHub app
 cd /d C:\PhoneHub
-start "" "C:\Users\User\AppData\Local\Python\pythoncore-3.14-64\pythonw.exe" "C:\PhoneHub\app\PhoneHub_Control_Center.py"
+start "" "C:\PhoneHub\dist\PhoneHub.exe"
+
 exit
