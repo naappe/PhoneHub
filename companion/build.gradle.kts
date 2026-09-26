36e22c36e8e833a7e5c33cb857b6a329968d7418
naappe <naappe@gmail.com>
2026-09-26 23:07:44 +0500

    Bump Companion to dev15 hybrid screen release

--- companion/build.gradle.kts
+++ companion/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.phonehub.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 82
-        versionName = "7.0.0-dev14"
+        versionCode = 83
+        versionName = "7.0.0-dev15"
     }
     buildTypes {
         getByName("release") {
