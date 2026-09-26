5a63aed8a55e9c95606803cc284b7270e2ebe531
naappe <naappe@gmail.com>
2026-09-26 22:30:29 +0500

    Bump Companion to dev13 Screen build

--- companion/build.gradle.kts
+++ companion/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.phonehub.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 80
-        versionName = "7.0.0-dev12"
+        versionCode = 81
+        versionName = "7.0.0-dev13"
     }
     buildTypes {
         getByName("release") {
