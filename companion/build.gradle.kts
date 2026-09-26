a9b516ece4a40629e0d47652035d126a0e1df2fd
naappe <naappe@gmail.com>
2026-09-26 15:41:51 +0500

    Add PhoneHub V7 two-way command channel

--- companion/build.gradle.kts
+++ companion/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.phonehub.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 71
-        versionName = "7.0.0-dev2"
+        versionCode = 72
+        versionName = "7.0.0-dev4"
     }
     buildTypes {
         getByName("release") {
