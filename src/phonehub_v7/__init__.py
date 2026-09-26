a23e5f910a388974e65c445b29bcbde4986a6958
naappe <naappe@gmail.com>
2026-09-26 13:22:33 +0500

    Start isolated PhoneHub 7 companion architecture

---
+++ src/phonehub_v7/__init__.py
@@ -0,0 +1,6 @@
+"""PhoneHub 7 experimental desktop components.
+
+Kept separate from the PhoneHub 6.3 runtime until the companion transport is
+proven stable on the target Android device.
+"""
+__version__ = "7.0.0-dev"
