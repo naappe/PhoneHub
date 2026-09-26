cd94e1c20e4afc5e13bb260c923eaca65b4e0774
naappe <naappe@gmail.com>
2026-09-26 13:23:20 +0500

    Add PhoneHub 7 Android companion lifecycle baseline

---
+++ companion/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.phonehub.companion"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.phonehub.companion"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 70
+        versionName = "7.0.0-dev1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+}
