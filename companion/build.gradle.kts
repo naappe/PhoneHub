1fe4d230811208e977e12940c4e8569383a70571
naappe <naappe@gmail.com>
2026-09-26 17:33:21 +0500

    Bump Companion to dev12 App Policy build

--- companion/build.gradle.kts
+++ companion/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.phonehub.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 79
-        versionName = "7.0.0-dev11"
+        versionCode = 80
+        versionName = "7.0.0-dev12"
     }
     buildTypes {
         getByName("release") {
